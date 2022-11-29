035f247f337c0bd4827e1a7e740d97795f38ef36
tayjanks <taylorjenkins801@gmail.com>
2022-11-28 19:16:27 -0700

    path work

--- client/main.js
+++ client/main.js
@@ -1,8 +1,11 @@
+const { default: axios } = require("axios")
+
 const collectionContainer = document.querySelector("#collection-continer")
 
 const baseURL = `http://localhost:3004`
 
 const collectionCallback = ({ data: collection }) => displayCollection(collection)
+const getAllAlbums = () => axios.get(baseURL).then(collectionCallback).catch(errCallback)
 const errCallback = err => console.log(err)
 
 document.getElementById('form-container').hidden = true;
@@ -21,6 +24,55 @@ const formContainer = document.querySelector("#form-container")
 addAlbumBtn = document.querySelector(".add-album-btn")
 
 
+
+
+function submitHandler(e) {
+    e.preventDefault()
+
+    let title = document.querySelector("#album-title")
+    let artist= document.querySelector("#artist")
+    let genre= document.querySelector("#genre")
+    let pressing= document.querySelector("#pressing")
+    let color= document.querySelector("#color")
+
+    let bodyObj = {
+        title: title.value,
+        artist: artist.value,
+        genre: genre.value,
+        pressing: pressing.value,
+        color: color.value
+       
+    }
+
+    createAlbum(bodyObj)
+    title.value = ''
+    artist.value = ''
+    genre.value = ''
+    pressing.value = ''
+    color.value = ''
+}
+
+
+function createAlbumCard(album){
+    const albumCard = document.createElement('div')
+    taskCard.classList.add('album-card')
+
+    taskCard.innerHTML = `<p class = "album-title" ${album.title}</p>
+    <p class = "album-artist" ${album.artist}</p>
+    <p class = "album-genre" ${album.genre}</p>
+    <p class = "album-pressing" ${album.pressing}</p>
+    <p class = "album-color"${album.color}</p>
+    `
+    collectionContainer.appendChild(albumCard)
+};
+
+function displayCollection (arr) {
+    collectionContainer.innerHTML = ``
+    for (let i=0; i<arr.length;i++){
+        createAlbumCard(arr[i])
+    }
+}
+
 function addAlbumForm () {
     imgContainer.remove()
     btnContainer.remove()
@@ -31,6 +83,7 @@ function showCollection () {
     imgContainer.remove()
     btnContainer.remove()
     document.getElementById('collection-continer').hidden=false;
+    //getAllAlbums()
 }
 
 addtoCollectionBtn.addEventListener("click", addAlbumForm)
